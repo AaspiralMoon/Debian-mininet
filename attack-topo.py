bc84df059d1fb4d4bd8af1d6d9a40d5d7880581c
Renjie Xu <93042200+AaspiralMoon@users.noreply.github.com>
2023-03-06 17:48:37 -0500

    Add files via upload

---
+++ attack-topo.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+                      ip='127.0.0.1',
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    att = net.addHost('att', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    leg = net.addHost('leg', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    vic = net.addHost('vic', cls=Host, ip='10.0.0.3', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(leg, s1)
+    net.addLink(s1, vic)
+    net.addLink(s1, att)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s1').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+    vic.cmd("/usr/sbin/inetd")
+    vic.cmd("/usr/sbin/sshd &")
+    vic.cmd("python3 -m http.server 80 &")
+    att.cmd("wireshark -i att-eth0 -k -Y '''ip.src == 10.0.0.0/24 or ip.dst == 10.0.0.0/24''' &")
+    CLI(net, script='xterm.sh')
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
